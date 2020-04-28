eac33d0b29086cb03d722be6caec95b39e6da1e0
Neskwik93 <33059627+Neskwik93@users.noreply.github.com>
2020-04-28 18:47:46 +0200

    initialisation database

---
+++ BDD/init.sql
@@ -0,0 +1,13 @@
+CREATE USER eric WITH PASSWORD 'toto123';
+
+ALTER USER eric WITH SUPERUSER;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    lastname VARCHAR(100),
+    firstname VARCHAR(100),
+    email VARCHAR(100),
+    pwd VARCHAR(100)
+);
+
+INSERT INTO users (lastname, firstname, email, pwd) VALUES ('Bomann', 'Eric', 'monadressemail@orange.com', 'password');
